d2a635dc5c4eff3284d73a863c22289266d73cf5
UserDebs <roumya2020second@gmail.com>
2024-06-21 16:09:21 +0530

    Deleting deploy requirements

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} >
-          <Route path="/" element={<Navigate to="/home"/>}/>
+          <Route index path="/" element={<Navigate to="/home"/>}/>
           <Route path="home" element={<Home />}/>
           <Route path="intro" element={<WhoWeAre />}/>
           <Route path="whatwedo" element={<WhatWeDo />}/>
