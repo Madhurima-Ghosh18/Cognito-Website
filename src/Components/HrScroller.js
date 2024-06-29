bba5622941e650e5cc290535df18e84011145b81
UserDebs <roumya2020second@gmail.com>
2024-06-29 22:35:04 +0530

    Adding Home Page Content

---
+++ src/Components/HrScroller.js
@@ -0,0 +1,8 @@
+const HrScroller = ({children}) => {
+    return (
+    <div className="scroll d-flex align-items-center gap-3 w-100">
+        {children}
+    </div>);
+}
+ 
+export default HrScroller;
